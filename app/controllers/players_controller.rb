03ab67f8e2bfcffa088f665c12ee996ad3bef71a
Jesse Rogers <djrquazz@gmail.com>
2016-12-05 23:42:56 -0500

    Doneski (for now)

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -18,8 +18,8 @@ class PlayersController < ApplicationController
              p2.name opponent, c2.value_string p2_class, r2.value_int p2_random
            FROM w3mmdplayers p1
            INNER JOIN w3mmdplayers p2 ON p1.gameid = p2.gameid AND p1.flag != p2.flag
-           LEFT JOIN w3mmdvars c1 ON p1.gameid = c1.gameid AND p1.pid = c1.pid AND c1.varname = 'class'
-           LEFT JOIN w3mmdvars c2 ON p2.gameid = c2.gameid AND p2.pid = c2.pid AND c2.varname = 'class'
+           LEFT JOIN w3mmdvars c1 ON p1.gameid = c1.gameid AND p1.pid = c1.pid AND c1.varname = 'class' AND c1.value_string != '""'
+           LEFT JOIN w3mmdvars c2 ON p2.gameid = c2.gameid AND p2.pid = c2.pid AND c2.varname = 'class' AND c1.value_string != '""'
            LEFT JOIN w3mmdvars r1 ON p1.gameid = r1.gameid AND p1.pid = r1.pid AND r1.varname = 'random'
            LEFT JOIN w3mmdvars r2 ON p2.gameid = r2.gameid AND p2.pid = r2.pid AND r2.varname = 'random'
            WHERE p1.name = '#{@name}'

945a082e0bbf91e33072afadeb0aaa46262c9190
Jesse Rogers <djrquazz@gmail.com>
2018-04-04 23:44:01 -0400

    Parse out the first part of the string

--- lib/parse_troll_class.rb
+++ lib/parse_troll_class.rb
@@ -1,5 +1,12 @@
+require 'is_numeric'
+
 module ParseTrollClass
   def parse_troll_class(str)
-    str[1...-1].split('_').drop(1).map(&:titleize).join(' ')
+    str[1...-1]
+      .split('_')
+      .drop(1)
+      .select { |x| !x.is_numeric? }
+      .map(&:titleize)
+      .join(' ')
   end
 end
